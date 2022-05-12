4edc7cac1d59d68cf29c7c610cb9911ee09585f8
Caro <caro@192.168.1.6>
2022-05-12 20:09:06 -0300

    Catalogo con Maps y Promises

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import Navbar from "./components/Navbar/Navbar";
+import {products} from "./data/products";
+import Item from "./components/Item/Item";
 
 
 function App() {
+
+  const task = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 3000)
+
+  })
+
+  task.then((result) => {
+    console.log(result)
+  }, err => {
+    console.log(err)
+  }).catch((err) => {
+    console.log(err)
+  })
+
+
   return (
     <div className="App">
       <Navbar />
-      <ItemListContainer count={"Computadora"}/> 
-    
+      <ItemListContainer count={"Computadora"} />
+
+      {products.map((products, index) => (
+        <Item product={products} key={products.id} />
+      )
+      )}
     </div>
   );
 }
---
+++ src/data/products.js
@@ -0,0 +1,30 @@
+export const products = [
+    {
+      id: 1, 
+      title: "AirPods",
+      stock: 3,
+      price: 250,
+      image: "img/NC3007.jpg"
+    },
+    {
+      id: 2, 
+      title: "Cargador Iphone",
+      stock: 2,
+      price: 50,
+      image: "img/NC3012.jpg"
+    },
+    {
+      id: 3, 
+      title: "Airpod Wireless HD",
+      stock: 10,
+      price: 500,
+      image: "img/NC3070.jpg"
+    },
+    {
+      id: 4, 
+      title: "Airphones Wire",
+      stock: 1,
+      price: 50,
+      image: "img/NC5034.jpg"
+    },
+  ]
